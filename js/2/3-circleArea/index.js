023fcad8355f4f634d7616b3729d958cf7163b60
Jordan Laor <jordanlaor@gmail.com>
2021-02-07 17:52:32 +0200

    js exercise 2.3 done

--- js/2/3-circleArea/index.js
+++ js/2/3-circleArea/index.js
@@ -4,6 +4,7 @@ function circleArea(radius) {
 
 const radius = 3;
 const area = circleArea(radius);
+// eslint-disable-next-line no-console
 console.log(
   `The area of a circle with a radius of ${radius} is ${area}.
   Rounded to 2 decimal places it should be ${area.toFixed(2)}`
