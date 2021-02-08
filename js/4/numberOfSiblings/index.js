50f338e404f075da712494058afbb46e747ada8d
Jordan Laor <jordanlaor@gmail.com>
2021-02-08 11:58:22 +0200

    js exercise 4.1 done

--- js/4/numberOfSiblings/index.js
+++ js/4/numberOfSiblings/index.js
@@ -1,5 +1,5 @@
 const numSiblings = prompt('How many siblings do you have?', '');
-if (numSiblings == 1) {
+if (Number(numSiblings) === 1) {
   console.log('1 sibling!');
 } else if (numSiblings > 1) {
   console.log('More than 1 sibling');
