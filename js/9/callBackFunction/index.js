d56e4de22344aafbd5702781f7e5255595c1a344
Jordan Laor <jordanlaor@gmail.com>
2021-02-14 13:19:24 +0200

    js exercise 9.1 done

--- js/9/callBackFunction/index.js
+++ js/9/callBackFunction/index.js
@@ -5,3 +5,12 @@ const isString = function (str, callBack) {
 
 console.log(isString('123', typeOf));
 console.log(isString([1, 2, 3], typeOf));
+
+function dasshed(str) {
+  return str.split(' ').join('-');
+}
+function firstWordUpperCase(str, callBack) {
+  return callBack(str.charAt(0).toUpperCase() + str.slice(1));
+}
+
+console.log(firstWordUpperCase('hi, im here!', dasshed));
