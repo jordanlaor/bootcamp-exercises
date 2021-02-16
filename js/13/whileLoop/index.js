4ccdf1f6be3360a82aa022864119fbb1d093ea36
Jordan Laor <jordanlaor@gmail.com>
2021-02-16 12:51:40 +0200

    js exercise 13.2 done

---
+++ js/13/whileLoop/index.js
@@ -0,0 +1,15 @@
+function populationPercentages(populations) {
+  const percentages = [];
+  while (populations.length) {
+    percentages.push(percentageOfWorld(populations.pop()));
+  }
+  return percentages;
+}
+
+function percentageOfWorld(population) {
+  return (population / 7900) * 100;
+}
+
+const populations = [9, 126, 60, 331];
+
+console.log(populationPercentages(populations));
