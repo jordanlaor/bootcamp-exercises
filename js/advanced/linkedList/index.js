8e09101518f647185b959d16b1954d08c326c137
Jordan Laor <jordanlaor@gmail.com>
2021-02-22 10:12:43 +0200

    js advanced list

--- js/advanced/linkedList/index.js
+++ js/advanced/linkedList/index.js
@@ -26,5 +26,85 @@ class LinkedList {
       this.last = this.last.next;
     }
     this.size += 1;
+    return this.last;
+  }
+
+  changeLastNext(element) {
+    this.last.next = element;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    return this.last;
+  }
+
+  draw() {
+    let current = this.head;
+    let value = '[Head] ==> ';
+    while (current) {
+      value += `[${current.toString()}] ==> `;
+      current = current.next;
+    }
+    return `${value}[Tail]`;
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    this.last = this.head;
+    let nextSave = current.next;
+    while (current) {
+      nextSave = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextSave;
+    }
+    this.head = prev;
+  }
+
+  removeKNode(k) {
+    let count = 0;
+    let current = this.head;
+    let previous = null;
+    while (count < k) {
+      previous = current;
+      current = current.next;
+      count += 1;
+    }
+    if (k === this.size - 1) {
+      this.last = previous;
+    }
+    if (k === 0) {
+      this.head = current.next;
+    } else {
+      previous.next = current.next;
+    }
+    this.size -= 1;
+  }
+
+  static circularLinkedList(listToCheck) {
+    let current = listToCheck.head;
+    const pointers = [];
+    while (current) {
+      pointers.push(current);
+      for (const pointer of pointers) {
+        if (pointer === current.next) {
+          return true;
+        }
+      }
+      current = current.next;
+    }
+    return false;
   }
 }
+
+const list = new LinkedList();
+const one = list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.changeLastNext(one);
