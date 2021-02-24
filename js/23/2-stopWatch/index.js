f013fbcf5ec2322f511faf532ba6908317819f5f
Jordan Laor <jordanlaor@gmail.com>
2021-02-24 21:10:06 +0200

    js ex 23 2

---
+++ js/23/2-stopWatch/index.js
@@ -0,0 +1,40 @@
+const startBtn = document.querySelector('#start-btn');
+const stopBtn = document.querySelector('#stop-btn');
+
+if (window.getComputedStyle(startBtn).width > window.getComputedStyle(stopBtn).width) {
+  stopBtn.style.width = window.getComputedStyle(startBtn).width;
+} else {
+  startBtn.style.width = window.getComputedStyle(stopBtn).width;
+}
+
+const mili = { element: document.querySelector('.mili'), counter: 0 };
+const secs = { element: document.querySelector('.secs'), counter: 0 };
+const mins = { element: document.querySelector('.mins'), counter: 0 };
+
+function timeCount(unit, countTo) {
+  if (mili.counter === countTo) {
+    unit.counter = 0;
+  } else {
+    unit.counter += 1;
+  }
+  unit.element.textContent = `${unit.counter}`.padStart(2, '0');
+}
+
+let miliInterval;
+let secsInterval;
+let minsInterval;
+
+startBtn.firstElementChild.addEventListener('input', (e) => {
+  console.log(e);
+  miliInterval = setInterval(() => timeCount(mili, 99), 1);
+  secsInterval = setInterval(() => timeCount(secs, 59), 1000);
+  minsInterval = setInterval(() => timeCount(mins, 59), 60000);
+});
+
+stopBtn.firstElementChild.addEventListener('input', () => {
+  if (miliInterval && secsInterval && minsInterval) {
+    clearInterval(miliInterval);
+    clearInterval(secsInterval);
+    clearInterval(minsInterval);
+  }
+});
