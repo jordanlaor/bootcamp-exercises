eca053ff9bfad5b405dc2aba2ac03685aae5a07f
Jordan Laor <jordanlaor@gmail.com>
2021-02-24 17:48:30 +0200

    js ex 22 4

---
+++ js/22/4-race/index.js
@@ -0,0 +1,19 @@
+const player1 = document.querySelector('#player1-race');
+const player2 = document.querySelector('#player2-race');
+
+function updateBoard(player) {
+  const active = player.querySelector('.active');
+  active.classList.remove('active');
+  active.nextElementSibling.classList.add('active');
+  if (active.nextElementSibling.classList.contains('finish')) {
+    window.removeEventListener('keyup', checkKey);
+    const title = document.querySelector('h1');
+    title.textContent += `   -   ${player.getAttribute('data-player')} Won`;
+  }
+}
+
+const checkKey = (e) => {
+  e.key === '1' ? updateBoard(player1) : e.key === '2' ? updateBoard(player2) : null;
+};
+
+window.addEventListener('keyup', checkKey);
