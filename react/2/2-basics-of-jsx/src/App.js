1bb7f5de35eb7947b533dd98327e0afbe637ea01
Jordan Laor <jordanlaor@gmail.com>
2021-03-14 17:09:38 +0200

    react ex2 done

--- react/2/2-basics-of-jsx/src/App.js
+++ react/2/2-basics-of-jsx/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const data = ["hello", "world"]; 
+  const number1 = 5;
+  const number2 =6;
+  const string = "I love React!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <div>{data.join(' ')}</div>
+     <div>{`${number1} + ${number2} = ${number1 + number2}`}</div>
+     <div>The string’s length is {string.length}</div>
     </div>
   );
 }
