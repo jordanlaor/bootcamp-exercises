2513e5e306dee2e8b5b4a403a1ced0b06358426d
Jordan Laor <jordanlaor@gmail.com>
2021-03-15 13:48:20 +0200

    react ex 4-2 done

--- react/4/2-cards/src/App.js
+++ react/4/2-cards/src/App.js
@@ -2,15 +2,15 @@ import Card from "./components/Card/Card.component";
 function App() {
   return (
     <div>
-      <Card image="https://source.unsplash.com/random" title="title1" description="loram lorem some random text">
+      <Card image="https://source.unsplash.com/random/800x600" title="title1" description="loram lorem some random text">
         <a href="#">link 1</a>
         <a href="#">link 2</a>
       </Card>
-      <Card image="https://source.unsplash.com/random" title="title2" description="why dont i have real lorem">
+      <Card image="https://source.unsplash.com/random/1000x700" title="title2" description="why dont i have real lorem">
         <a href="#">link 3</a>
         <a href="#">link 4</a>
       </Card>
-      <Card image="https://source.unsplash.com/random" title="title3" description="i have no idea what random text should i write">
+      <Card image="https://source.unsplash.com/random/900x800" title="title3" description="i have no idea what random text should i write">
         <a href="#">link 5</a>
         <a href="#">link 6</a>
         <a href="#">link 7</a>
