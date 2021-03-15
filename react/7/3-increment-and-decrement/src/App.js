0b33a0e8ed9230b285b1baad14e30c9e9b7ed7e6
Jordan Laor <jordanlaor@gmail.com>
2021-03-15 21:37:08 +0200

    react ex 7-3 done

---
+++ react/7/3-increment-and-decrement/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import "./button.css";
+class Button extends React.Component {
+  render() {
+    return <button onClick={this.props.action}>{this.props.text}</button>;
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  increase() {
+    this.state.counter < 10 && this.setState({ counter: this.state.counter + 1 });
+  }
+
+  decrease() {
+    this.state.counter > -10 && this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button action={this.increase.bind(this)} text="+" />
+        <div
+          className="number"
+          style={(this.state.counter < 0 && { color: "red" }) || (this.state.counter > 0 && { color: "green" }) || { color: "black" }}
+        >
+          {this.state.counter}
+        </div>
+        <Button action={this.decrease.bind(this)} text="-" />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/7/3-increment-and-decrement/src/components/Button/Button.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./button.css";
+class Button extends React.Component {
+  render() {
+    return <button onClick={this.props.action}>{this.props.text}</button>;
+  }
+}
+
+export default Button;
