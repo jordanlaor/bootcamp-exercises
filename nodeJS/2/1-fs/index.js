c34808dace922bb6dd65688ecec469ee35999272
Jordan Laor <jordanlaor@gmail.com>
2021-04-11 15:23:06 +0300

    starting node js, finished ex 2-1

---
+++ nodeJS/2/1-fs/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+fs.writeFileSync("./fsNewFile.txt", "fs new file content");
+fs.copyFileSync("./fsNewFile.txt", "fsCopyFile.txt");
+fs.renameSync("./fsNewFile.txt", "./fsNewNameFile.txt");
+console.log(fs.readdirSync(".", { withFileTypes: true }));
+console.log(fs.readFileSync("./fsCopyFile.txt"));
